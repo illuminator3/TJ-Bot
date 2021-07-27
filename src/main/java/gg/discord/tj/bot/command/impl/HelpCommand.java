2909f2d87d137095c208495057a1338873e9399b
illuminator3 <48388251+illuminator3@users.noreply.github.com>
2021-07-27 20:46:17 +0200

    Add separator

--- src/main/java/gg/discord/tj/bot/command/impl/HelpCommand.java
+++ src/main/java/gg/discord/tj/bot/command/impl/HelpCommand.java
@@ -5,18 +5,17 @@ import discord4j.core.object.entity.Message;
 import gg.discord.tj.bot.command.Command;
 import gg.discord.tj.bot.command.CommandExecutionContext;
 import gg.discord.tj.bot.repository.CommandRepository;
-import gg.discord.tj.bot.service.MessageService;
 import gg.discord.tj.bot.util.PresentationUtils;
 import reactor.core.publisher.Mono;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static gg.discord.tj.bot.util.MessageTemplate.PLAINTEXT_MESSAGE_TEMPLATE;
 
 public class HelpCommand implements Command {
     private final static CommandRepository COMMAND_REPOSITORY = CommandRepository.INSTANCE;
-    public static final MessageService MESSAGE_SERVICE = new MessageService();
 
     @Override
     public String getName() {
@@ -36,15 +35,15 @@ public class HelpCommand implements Command {
     @Override
     public Mono<Void> onExecute(CommandExecutionContext context) {
         Message message = context.message();
-        
+
         return message
             .getChannel()
             .flatMap(channel -> channel == null ?
                 Mono.empty() :
                 channel.createMessage(String.format(PLAINTEXT_MESSAGE_TEMPLATE, PresentationUtils.dataFrameToAsciiTable(
                     fetchCommandInfoDataFrame(),
-                    new String[] {"Command", "Alias", "Description"},
-                    new HorizontalAlign[] {HorizontalAlign.LEFT, HorizontalAlign.LEFT, HorizontalAlign.LEFT}
+                    new String[] { "Command", "Aliases", "Description" },
+                    new HorizontalAlign[] { HorizontalAlign.LEFT, HorizontalAlign.LEFT, HorizontalAlign.LEFT }
                 )))
                 ).then();
     }
@@ -52,6 +51,8 @@ public class HelpCommand implements Command {
     private List<List<String>> fetchCommandInfoDataFrame() {
         return COMMAND_REPOSITORY.commands().stream()
             .map(command -> List.of(command.getName(), command.getAliases().toString().replaceAll("^.|.$", ""), command.getDescription()))
+            .flatMap(l -> Stream.of(List.of("", "", ""), l))
+            .skip(1)
             .toList();
     }
 }
